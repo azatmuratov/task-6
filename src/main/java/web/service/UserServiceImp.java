ace98f0d46f4d5a6d7c14107718fe2a947497d73
gersogit <gersogit@gmail.com>
2023-10-28 01:23:50 +0500

    project

---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,47 @@
+package web.service;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void removeUser(long id) {
+        userDao.removeUser(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+}
